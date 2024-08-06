de095187f73e776a38c3ab4e08eaf70b4332283d
Aditya Raj <adistrim.dev@gmail.com>
2024-08-06 09:38:27 +0530

    collecting user-details from backend and displaying it on the frontend

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -7,6 +7,7 @@ const HomePage = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [, setToken] = useState("");
+  const [name, setName] = useState("");
   const router = useRouter();
 
   useEffect(() => {
@@ -29,6 +30,7 @@ const HomePage = () => {
         );
         if (response.ok) {
           setIsLoggedIn(true);
+          fetchUserDetails(token);
         } else {
           localStorage.removeItem("token");
           router.push("/login");
@@ -43,10 +45,26 @@ const HomePage = () => {
     verifyToken();
   }, [router]);
 
-  const handleLogin = (newToken) => {
-    localStorage.setItem("token", newToken);
-    setToken(newToken);
-    setIsLoggedIn(true);
+  const fetchUserDetails = async (token) => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/user-details`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setName(data.name);
+      } else {
+        console.error("Failed to fetch user details");
+      }
+    } catch (error) {
+      console.error("Error fetching user details:", error);
+    }
   };
 
   const handleLogout = () => {
@@ -68,7 +86,7 @@ const HomePage = () => {
     <div className="min-h-screen bg-gradient-to-br from-purple-600 to-blue-500">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <nav className="flex justify-between items-center py-6">
-          <h1 className="text-3xl font-bold text-white">Home Page</h1>
+          <h1 className="text-3xl font-bold text-white">Event Sync</h1>
           <button
             onClick={handleLogout}
             className="bg-white text-indigo-600 px-4 py-2 rounded-md font-medium hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
@@ -79,7 +97,7 @@ const HomePage = () => {
         <main className="mt-10">
           <div className="bg-white p-8 rounded-lg shadow-xl">
             <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-              Welcome to Your Dashboard
+              Hi {name}, Welcome to Your Dashboard
             </h2>
             <p className="text-gray-600">You&apos;re successfully logged in!</p>
           </div>
